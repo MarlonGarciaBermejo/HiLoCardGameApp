96c1d3dc6e4d6acc20b5bb16fb831009a795a620
Marlon Garcia <marlongarciab@hotmail.com>
2022-11-17 14:40:16 +0100

    Update MainActivity.kt
    Added click function on start button to enter to second game activity.

--- app/src/main/java/com/firstapp/hilocardgameapp/MainActivity.kt
+++ app/src/main/java/com/firstapp/hilocardgameapp/MainActivity.kt
@@ -7,5 +7,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        val buttonClick = findViewById<Button>(R.id.startButton)
+        buttonClick.setOnClickListener {
+            val intent = Intent(this, GameActivity::class.java)
+            startActivity(intent)
     }
 }
