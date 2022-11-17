0a7505f19a8935e4e3663050288b818b5a24164f
Marlon Garcia <marlongarciab@hotmail.com>
2022-11-17 15:28:17 +0100

    Updated GameActivity.kt
    Added a back button to go back to main menu from second activity.

--- app/src/main/java/com/firstapp/hilocardgameapp/GameActivity.kt
+++ app/src/main/java/com/firstapp/hilocardgameapp/GameActivity.kt
@@ -9,5 +9,8 @@ class GameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.game_activity)
+        
+          supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+          //Back button to go back to main menu(activity 1)
     }
 }
