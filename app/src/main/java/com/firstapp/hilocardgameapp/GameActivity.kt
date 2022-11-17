4db57e48f230c26eb81815391f29f381252b354c
markopaunovic1 <marpau970514@hotmail.com>
2022-11-17 16:41:12 +0100

    Imported card deck images, view number on the right

--- app/src/main/java/com/firstapp/hilocardgameapp/GameActivity.kt
+++ app/src/main/java/com/firstapp/hilocardgameapp/GameActivity.kt
@@ -2,12 +2,19 @@ package com.firstapp.hilocardgameapp
 import android.os.Bundle
 import android.util.Log
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 
 class GameActivity : AppCompatActivity() {
+
+    lateinit var showNumber: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.game_activity)
+
+        showNumber = findViewById(R.id.showNumberView)
+
     }
 }
